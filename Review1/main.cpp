c9e6e251605fe9706f305625818c8af02d670a11
Breno Sallouti <brenosallouti@icloud.com>
2017-05-30 18:09:09 -0300

    Finished Task 1

--- Review1/main.cpp
+++ Review1/main.cpp
@@ -12,7 +12,11 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int i;
+    i=10;
+    int x;
+    x =i ;
+    
+    std::cout << x;
     return 0;
 }
